853d5e39f94ea9fa4b6ca0dd2ad4de0c93256903
george@labrys.tech <106515238+georgeLabrysGroup@users.noreply.github.com>
2023-08-07 10:54:49 +0100

    Custom answer button

--- adv_basics_quiz/lib/screens/questions_screen.dart
+++ adv_basics_quiz/lib/screens/questions_screen.dart
@@ -1,3 +1,4 @@
+import 'package:adv_basics_quiz/widgets/answer_button.dart';
 import 'package:flutter/material.dart';
 
 class QuestionsScreen extends StatefulWidget {
@@ -10,10 +11,30 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text(
-      'Question Screen',
-      style: TextStyle(color: Colors.white),
-    ));
+    return SizedBox(
+      //Be as wide as possible
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'the question..',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answerText: 'Answer 1 text ..', onTap: () {}),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answerText: 'Answer 2 text ..', onTap: () {}),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answerText: 'Answer 3 text ..', onTap: () {}),
+        ],
+      ),
+    );
   }
 }
